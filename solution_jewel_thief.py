fb5adfe59c9fe35ab2337f8dcafc40adc592803f
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-09-29 11:42:58 +0530

    Update solution_jewel_thief.py

--- solution_jewel_thief.py
+++ solution_jewel_thief.py
@@ -42,24 +42,42 @@ jewel_image = pygame.transform.scale(jewel_image, (50,50))
 
 while True:
     screen.fill((100,255,255))
+    
+    # Checking if 'quit' event occurs and handle it by calling 'pygame.quit()'
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
+            
+                # Checking if left arrow key is pressed
                 if event.key == pygame.K_LEFT:
+                    # Making 'thief_x_change' value to -1
                     thief_x_change = -1
+                    
+                # Checking if right arrow key is pressed    
                 if event.key == pygame.K_RIGHT:
+                    # Making 'thief_x_change' value to 1
                     thief_x_change = 1
+                    
+                # Checking if down arrow key is pressed
                 if event.key == pygame.K_DOWN:
+                    # Making 'thief_y_change' value to 1
                     thief_y_change = 1
+                    
+                # Checking if up arrow key is pressed 
                 if event.key == pygame.K_UP:
+                    # Making 'thief_y_change' value to -1
                     thief_y_change = -1
+                    
         if event.type == pygame.KEYUP:
             thief_x_change = 0
             thief_y_change = 0
-                    
+            
+    # Updating the x,y values of the thief
+    # Incrementing 'thief.rect.x' with the 'thief_x_change' value                
     thief.rect.x += thief_x_change
+    # Incrementing 'thief.rect.y' with the 'thief_y_change' value
     thief.rect.y += thief_y_change
     
     if hurdle_1.rect.y > 398:
@@ -75,6 +93,8 @@ while True:
     thief.draw_thief(screen)
     screen.blit(jewel_image, jewel)
     
+    # Checking if 'hurdle_1.rect' or 'hurdle_2.rect' collides with 'thief.rect' 
+    # Using 'or' to combine the 2 conditions
     if hurdle_1.rect.colliderect(thief.rect) or hurdle_2.rect.colliderect(thief.rect):
         text = font.render("Failed, you lost", True, (0,0,0))
         screen.blit(text, (125,175))
@@ -82,7 +102,8 @@ while True:
         pygame.time.delay(2000)
         pygame.quit()
         sys.exit()
-    
+        
+    # Checking if 'thief.rect' collides with 'jewel'
     if thief.rect.colliderect(jewel):
         text = font.render("Congratulations, you won", True, (0,0,0))
         screen.blit(text, (125,175))
